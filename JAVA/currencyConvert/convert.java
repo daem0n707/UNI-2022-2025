0a16176b576cd45b365d0f78127dca66c6918585
cheesecake <blueberry.cheesecakes75@gmail.com>
2023-01-16 16:14:42 +0000

    currency

---
+++ JAVA/currencyConvert/convert.java
@@ -0,0 +1,14 @@
+import convertFunctions.currencyConvert;
+import convertFunctions.timeConvert;
+
+public class convert {
+
+	public static void main(String[] args) {
+		timeConvert t = new timeConvert();
+		currencyConvert c = new currencyConvert();
+		t.htom();
+		c.rtod();
+	}
+
+}
+	
--- JAVA/currencyConvert/mainClass.java
+++
@@ -1,9 +0,0 @@
-import currencyConvert.convertClass;
-
-public class mainClass{
-    public static void main(String[] args){
-    convertClass d = new convertClass();
-    d.dtor();
-    }
-}
-
