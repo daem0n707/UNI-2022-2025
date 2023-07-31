0b10a7a0c570ace566129d6cf4014cef6452d9d4
Fbi <fbi@rohannk.com>
2023-07-31 22:20:17 +0530

    Roman & Phone Number

--- 4th_SEM/PYTHON/P5/roman.py
+++ 4th_SEM/PYTHON/P5/roman.py
@@ -3,7 +3,7 @@ num = input("Enter Roman numeral: ")
 def r_to_d(r):
     roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     total = pre_value = 0
-    for i in r[::-1]:
+    for i in r[::-1]: #IIX
         curr_value = roman[i]
         if curr_value>=pre_value:
             total += curr_value
@@ -13,4 +13,4 @@ def r_to_d(r):
     return total
     
     
-r_to_d(roman)
+print(f"Decimal: {r_to_d(num)}")
