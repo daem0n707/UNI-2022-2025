5d6d63d06b5bea1a2e24c2485508208ea75966da
Fbi <fbi@rohannk.com>
2023-08-03 18:27:34 +0530

    Knapsack

---
+++ 4th_SEM/DAA/knapsack.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] weight = new int[n];
+        int[] profit = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the profit: ");
+            profit[i] = sc.nextInt();
+            System.out.println("Enter the weight: ");
+            weight[i] = sc.nextInt();
+        }
+        System.out.println("Enter the Knapsack capacity: ");
+        int bag = sc.nextInt();
+        double[] pw = new double[n];
+        for (int j = 0; j < n; j++) {
+            pw[j] = (double) profit[j] / (double) weight[j];
+        }
+        for (int x = 0; x < n; x++) {
+            for (int y = x + 1; y < n;y++ ) {
+                if (pw[x] < pw[y]) {
+                    double t1 = pw[x];
+                    pw[x] = pw[y];
+                    pw[y] = t1;
+
+                    int t2 = profit[x];
+                    profit[x] = profit[y];
+                    profit[y] = t2;
+
+                    int t3 = weight[x];
+                    weight[x] = weight[y];
+                    weight[y] = t3;
+                }
+            }
+        }
+
+        int k = 0;
+        double sum = 0;
+        while (bag > 0) {
+            if (weight[k] < bag) {
+                sum += 1 * profit[k];
+                bag = bag - weight[k];
+                k++;
+            } else {
+                sum = sum + (bag * pw[k]);
+                bag = 0;
+            }
+        }
+        System.out.println(Arrays.toString(profit));
+        System.out.println(Arrays.toString(weight));
+        System.out.println(Arrays.toString(pw));
+        System.out.println(sum);
+    }
+}
