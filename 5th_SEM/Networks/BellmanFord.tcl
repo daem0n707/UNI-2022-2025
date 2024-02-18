a7cc9c0c07488731b240f390ad05ddc68354af15
Fbi <fbi@rohannk.com>
2024-02-18 21:39:53 +0530

    BellmanFord Algorithm

--- 5th_SEM/Networks/BellmanFord.tcl
+++ 5th_SEM/Networks/BellmanFord.tcl
@@ -1,85 +1,70 @@
-set ns [new Simulator]
-set nf [open lan.nam w]
-$ns namtrace-all $nf
-set tf [open lan.tr w]
-$ns trace-all $tf
+import java.util.*;
 
-set n0 [$ns node]
-$n0 color "magenta"
-$n0 label "src1"
+public class misc {
+    private int D[];
+    private int n;
+    public static final int max_value = 999;
 
-set n1 [$ns node]
-set n2 [$ns node]
-$n2 color "magenta"
-$n2 label "src2"
+    public misc(int n) {
+        this.n = n;
+        D = new int[n + 1];
+    }
 
-set n3 [$ns node]
-$n3 color "blue"
-$n3 label "dest2"
+    public void shortest(int s, int a[][]) {
+        for (int i = 1; i <= n; i++) {
+            D[i] = max_value;
+        }
+        D[s] = 0;
 
-set n4 [$ns node]
-set n5 [$ns node]
-$n5 color "blue"
-$n5 label“dest1”
+        for (int k = 1; k <= n - 1; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (a[i][j] != max_value) {
+                        if (D[j] > D[i] + a[i][j])
+                            D[j] = D[i] + a[i][j];
+                    }
+                }
+            }
+        }
 
-$ns make-lan "$n0 $n1 $n2 $n3 $n4" 100Mb 100ms LL Queue/DropTail Mac/802_3
-$ns duplex-link $n4 $n5 1Mb 1ms DropTail
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a[i][j] != max_value) {
+                    if (D[j] > D[i] + a[i][j]) {
+                        System.out.println("\nThe graph contains negative edge cycle");
+                        return;
+                    }
+                }
+            }
+        }
 
-set tcp0 [new Agent/TCP]
-$ns attach-agent $n0 $tcp0
-set ftp0 [new Application/FTP]
-$ftp0 attach-agent $tcp0
-$ftp0 set packetSize_ 500
-$ftp0 set interval_ 0.0001
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Distance of source " + s + " to " + i + " is " + D[i]);
+        }
+    }
 
-set sink5 [new Agent/TCPSink]
-$ns attach-agent $n5 $sink5
-$ns connect $tcp0 $sink5
-
-set tcp2 [new Agent/TCP]
-$ns attach-agent $n2 $tcp2
-set ftp2 [new Application/FTP] $ftp2
-attach-agent $tcp2 $ftp2 set
-packetSize_ 600 $ftp2 set
-interval_ 0.001
-
-set sink3 [new Agent/TCPSink] $ns
-attach-agent $n3 $sink3 $ns
-connect $tcp2 $sink3
-
-set file1 [open file1.tr w]
-$tcp0 attach $file1
-set file2 [open file2.tr w]
-$tcp2 attach $file2
-
-$tcp0 trace cwnd_
-$tcp2 trace cwnd_
-
-proc finish { } {
-global ns nf tf
-$ns flush-trace
-close $tf
-close $nf
-exec nam lan.nam &
-exec cat file1.tr > a1
-exec cat file2.tr > a2
-exec xgraph a1 a2
-exit 0
+    public static void main(String[] args) {
+        int n = 0, s;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of nodes: ");
+        n = sc.nextInt();
+        int[][] a = new int[n + 1][n + 1];
+        System.out.println("\nEnter the weighted matrix:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                a[i][j] = sc.nextInt();
+                if (i == j) {
+                    a[i][j] = 0;
+                    continue;
+                }
+                if (a[i][j] == 0)
+                    a[i][j] = max_value;
+            }
+        }
+        System.out.print("\nEnter the source vertex: ");
+        s = sc.nextInt();
+        misc b = new misc(n);
+        b.shortest(s, a);
+        sc.close();
+    }
 }
-$ns at 0.1 "$ftp0 start"
-$ns at 5 "$ftp0 stop"
-$ns at 7 "$ftp0 start"
-$ns at 0.2 "$ftp2 start"
-$ns at 8 "$ftp2 stop"
-$ns at 14 "$ftp0 stop"
-$ns at 10 "$ftp2 start"
-$ns at 15 "$ftp2 stop"
-$ns at 16 "finish"
-$ns run
-
-congestion window trace files i.e. file1.tr and file2.tr
-awk –f lan.awk file1.tr &gt; a1
-awk –f lan.awk file2.tr &gt; a2
-xgraph a1 a2
-
-See trace file contents: vi lan.tr
